650d1a80e02ccde2319699bada451566c8b22e8c
jose.rubio5317@alumnos.udg.mx <jose.rubio5317@alumnos.udg.mx>
2021-03-23 14:49:18 -0600

    Eliminamos los experimentos del archivo conjuntos

--- conjunto.py
+++ conjunto.py
@@ -13,11 +13,3 @@ class Conjunto:
     def mostrar(self):
         for particula in self.__particulas:
             print(particula)
-
-p01 = Particula(id=101,origen_X=5,origen_Y=8,destino_X=3,destino_Y=6,velocidad=13,red=111,green=186,blue=50)
-p02 = Particula(206,15,6,26,5,22,42,150,206)
-conjunto = Conjunto()
-conjunto.agregar_final(p01)
-conjunto.agregar_inicio(p02)
-conjunto.agregar_inicio(p01)
-conjunto.mostrar()
